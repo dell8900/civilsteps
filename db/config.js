d2d9f6c686c79ff460eba924a77b64fc60f892f1
Pushpendra Singh <90454357+erpushpendrasingh@users.noreply.github.com>
2025-05-08 10:39:32 +0530

    u

--- db/config.js
+++ db/config.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Connected to the database successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1); // Exit process with failure
-    }
+ try {
+  await mongoose.connect(
+   "mongodb+srv://dellinspiron8900:1234@cluster0.djudhkd.mongodb.net/civilsteps?retryWrites=true&w=majority&appName=Cluster0",
+   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   }
+  );
+  console.log("Connected to the database successfully");
+ } catch (error) {
+  console.error("MongoDB connection error:", error.message);
+  process.exit(1); // Exit process with failure
+ }
 };
 
 module.exports = connectDB;
